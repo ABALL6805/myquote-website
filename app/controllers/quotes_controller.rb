888fe9543fb94021ef2faba36aef91cbd6869b6b
aballz <aballing@our.ecu.edu.au>
2025-10-13 10:24:40 +0000

    "Fixed editting and seeded more data"

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -35,7 +35,7 @@ class QuotesController < ApplicationController
     author = Author.find_or_create_by(fname: fname, lname: author_params[:lname], birth_year: author_params[:birth_year], death_year: author_params[:death_year]) # Copilot code - How do I check for an existing author before creating one?
     @quote = Quote.new(quote_params)
     @quote.author = author  # Copilot code - How do I check for an existing author before creating one?
-    @quote.biography.author = author
+    @quote.biography.author = author if @quote.biography.present?
 
     respond_to do |format|
       if @quote.save
@@ -53,10 +53,15 @@ class QuotesController < ApplicationController
     # Prevents unauthorised user editing 
     if logged_in? && (@quote.user_id == current_user.id || is_administrator?)
       author_params = quote_params.delete(:author_attributes)  # Copilot code - How do I check for an existing author before creating one?
+
+      # Copilot code - How do I set the Author's first name to anonymous if the text field is left blank?
+      fname = author_params[:fname]
+      fname = "Anonymous" if fname.blank?
+
       author = Author.find_or_create_by(fname: author_params[:fname], lname: author_params[:lname], birth_year: author_params[:birth_year], death_year: author_params[:death_year]) # Copilot code - How do I check for an existing author before creating one?
-      @quote = Quote.new(quote_params)
+      @quote.assign_attributes(quote_params)
       @quote.author = author  # Copilot code - How do I check for an existing author before creating one?
-      @quote.biography.author = author
+      @quote.biography.author = author if @quote.biography.present?
 
       respond_to do |format|
         if @quote.update(quote_params)
@@ -97,11 +102,19 @@ class QuotesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def quote_params
-      params.require(:quote).permit(
+      permitted = [
       :quote, :pub_year, :comment, :is_public, :user_id,
       author_attributes: [:fname, :lname, :birth_year, :death_year],   # Copilot code - How do I create fields for a the author model in a quote form?
-      category_quotes_attributes: [:category_id, :id, :_destroy],
       biography_attributes: [:biography]
-    )
+      ]
+      
+      # Copilot code - How to fix an error relating to quote id and category quotes when editing a quote
+      if action_name == "update" || action_name == "edit"
+        permitted << {category_quotes_attributes: [:category_id, :id, :_destroy]}
+      else
+        permitted << {category_quotes_attributes: [:category_id]}
+      end
+
+      params.require(:quote).permit(permitted)
   end
 end
